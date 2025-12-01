bb80f2aafbec09c592fc4cf234d99caf039a5855
raulfrizeru <raulconstantin.raulea@ulbsibiu.ro>
2025-12-01 15:39:42 +0200

    Updated main_module.py

--- ai-module/src/data_extraction.py
+++ ai-module/src/data_extraction.py
@@ -2,7 +2,6 @@ from docling.document_converter import DocumentConverter
 import os
 
 os.environ["HF_TOKEN"] = "hf_"
-source = r"C:\Users\raulr\Desktop\LICENTA\SkillMatch\ai-module\testing_files\CV.pdf"
 
 def extract_text_from_pdf(file_path):
     converter = DocumentConverter()
